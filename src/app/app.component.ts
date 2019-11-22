b338f2b916c68ccf2c91b76601d14d6568aa3748
Dasun Kasthuri <dasun_k@3logixs.lk>
2019-11-22 13:43:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-first-angular-app';
+  name = 'dasun';
+  ServerName = 'apollo';
+  ServerPID = 11;
+  ServerStatus = 'offline';
+  statusFlag = false;
+  buttonState = true;
+  flag = true;
+  color = "black";
+
+  constructor() {
+    setTimeout(()=>{
+      this.buttonState = false;
+    }, 2500);
+  }
+
+  toggleServerStatus() {
+    this.statusFlag = !this.statusFlag;
+    if(this.statusFlag === true) {
+      this.ServerStatus = 'online'
+    }
+    else{
+      this.ServerStatus = 'offline'
+    }
+    return this.ServerStatus
+  }
+
+  getColor() {
+    if(this.statusFlag === true) {
+      this.color = 'red'
+    }
+    else{
+      this.color = 'gray'
+    }
+    return this.color
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './servers/server.component';
+import { RedComponent } from './red/red.component';
+import { BlueComponent } from './blue/blue.component';
+import { GreenComponent } from './green/green.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    RedComponent,
+    BlueComponent,
+    GreenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
